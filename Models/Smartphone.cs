36ca93b6539c72f47635e044ee96d4cc7979d4bc
Douglas <douglas.ferreirag@gmail.com>
2022-10-05 19:42:27 -0300

    Resolução do exercício proposto

---
+++ Models/Smartphone.cs
@@ -0,0 +1,33 @@
+namespace DesafioPOO.Models
+{
+    public abstract class Smartphone
+    {
+        public string Numero { get; set; }
+        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
+        {
+            Numero = numero;
+            // TODO: Passar os parâmetros do construtor para as propriedades
+             Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+        }
+
+        public void Ligar()
+        {
+            Console.WriteLine("Ligando...");
+        }
+
+        public void ReceberLigacao()
+        {
+            Console.WriteLine("Recebendo ligação...");
+        }
+
+        public abstract void InstalarAplicativo(string nomeApp);
+    }
+}
